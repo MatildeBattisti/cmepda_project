ff40b88a5688fcf9d1ae6f489aca5215d1beb002
Matilde <battisti.matilde@gmail.com>
2025-04-18 20:42:28 +0200

    General decluttering

--- README.md
+++ README.md
@@ -21,3 +21,25 @@ On this note data_skimming.C only keeps MET and Jet entries, excluding trigger a
 
 ## The Machine Learning algorithm
 The MET resolution is improved through a ML regression algorithm, using XGBoost packages.
+The aim of this project is to implement a machine learning algorithm to improve the Missing Energy (MET) resolution in LHC experiments using CERN Open Data.
+
+## Requirements
+To run this code you may need to install the following packages.
+
+'''bash
+pip install xgboost
+pip install uproot
+'''
+
+Alternatively, since we are not using an NVIDIA GPU you can install the lighter xgboost package. This is not recommended since some functionalities may not work.
+
+'''bash
+pip install xgboost-cpu
+'''
+
+## Exploring the dataset
+In the data_retrieving.C file we read the Open Data .root file, particularly exploring the branches inside the 'Event' TTree.
+This will be useful to decide what entries are not useful to pass as parameters of the ML algorithm, such as flag and trigger bits.
+data_skimming.C gets rid of such uniteresting branches.
+
+## Machine Learning alorithm
--- data_retrieving.C
+++ data_retrieving.C
@@ -7,8 +7,8 @@ void data_retrieving() {
      * @brief Open file from local path.
      * Returns an error if file is not opened.
      */
-    //TFile *file = TFile::Open("/home/matilde/Downloads/1C037D8D-2092-2448-81A4-BE32B05BFB45.root");
-    TFile *file = TFile::Open("/home/matilde/Documenti/cmepda_project/skimmed.root");
+    //TFile *file = TFile::Open("datasets/2E96A5E9-C938-A149-BBBF-8FD81A9E5AD6.root");
+    TFile *file = TFile::Open("datasets/skimmed.root");
 
     if (!file || file->IsZombie()) {
         std::cerr << "Error opening file." << std::endl;
@@ -18,7 +18,7 @@ void data_retrieving() {
     
     /**
      * @brief Get 'Events' tree entries.
-     * Default -> EVENT:-1. We only use it to see the entries name.
+     * If Show() is left empty, shows EVENT:-1. We only use it to see the entries name.
      * Returns error if the tree isn't loaded.
      */
     TTree *event = (TTree*)file->Get("Events");
@@ -29,7 +29,7 @@ void data_retrieving() {
         exit(-1);
     }
 
-    event->Show();
+    event->Show(1);
 
     /**
      * @brief Gets number of branches inside the 'Events' TTree.
