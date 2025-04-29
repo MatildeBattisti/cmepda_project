91c8a7ccacf9c66a982ea8141425e2a7b3fe47fc
Matilde <battisti.matilde@gmail.com>
2025-04-29 18:23:09 +0200

    Added logarithms of branches

--- data_skimming.C
+++ data_skimming.C
@@ -82,13 +82,26 @@ void data_skimming() {
     newtree->Branch("MET_significance", &MET_significance);
 
     /**
+     * @brief Log array for MET_pt entry.
+     */
+    Float_t MET_pt_log;
+    newtree->Branch("MET_pt_log", &MET_pt_log);
+    MET_pt_log = std::log(std::fabs(MET_pt)+1);
+
+    /**
      * @brief Selects only the first three jets and
-     * defines them as new branches
+     * defines them as new branches.
+     * Creates log array for each entry.
      */
     Float_t Jet_eta_bst, Jet_eta_bnd, Jet_eta_brd;
     Float_t Jet_pt_bst, Jet_pt_bnd, Jet_pt_brd;
     Float_t Jet_phi_bst, Jet_phi_bnd, Jet_phi_brd;
     Float_t Jet_mass_bst, Jet_mass_bnd, Jet_mass_brd;
+
+    Float_t Jet_eta_bst_log, Jet_eta_bnd_log, Jet_eta_brd_log;
+    Float_t Jet_pt_bst_log, Jet_pt_bnd_log, Jet_pt_brd_log;
+    Float_t Jet_phi_bst_log, Jet_phi_bnd_log, Jet_phi_brd_log;
+    Float_t Jet_mass_bst_log, Jet_mass_bnd_log, Jet_mass_brd_log;
 
     // Best Jet
     newtree->Branch("Jet_eta_bst", &Jet_eta_bst);
@@ -96,11 +109,21 @@ void data_skimming() {
     newtree->Branch("Jet_phi_bst", &Jet_phi_bst);
     newtree->Branch("Jet_mass_bst", &Jet_mass_bst);
 
+    newtree->Branch("Jet_eta_bst_log", &Jet_eta_bst_log);
+    newtree->Branch("Jet_pt_bst_log", &Jet_pt_bst_log);
+    newtree->Branch("Jet_phi_bst_log", &Jet_phi_bst_log);
+    newtree->Branch("Jet_mass_bst_log", &Jet_mass_bst_log);
+
     // Second best Jet
     newtree->Branch("Jet_eta_bnd", &Jet_eta_bnd);
     newtree->Branch("Jet_pt_bnd", &Jet_pt_bnd);
     newtree->Branch("Jet_phi_bnd", &Jet_phi_bnd);
     newtree->Branch("Jet_mass_bnd", &Jet_mass_bnd);
+
+    newtree->Branch("Jet_eta_bnd_log", &Jet_eta_bnd_log);
+    newtree->Branch("Jet_pt_bnd_log", &Jet_pt_bnd_log);
+    newtree->Branch("Jet_phi_bnd_log", &Jet_phi_bnd_log);
+    newtree->Branch("Jet_mass_bnd_log", &Jet_mass_bnd_log);
 
     // Third best Jet
     newtree->Branch("Jet_eta_brd", &Jet_eta_brd);
@@ -108,28 +131,50 @@ void data_skimming() {
     newtree->Branch("Jet_phi_brd", &Jet_phi_brd);
     newtree->Branch("Jet_mass_brd", &Jet_mass_brd);
 
+    newtree->Branch("Jet_eta_brd_log", &Jet_eta_brd_log);
+    newtree->Branch("Jet_pt_brd_log", &Jet_pt_brd_log);
+    newtree->Branch("Jet_phi_brd_log", &Jet_phi_brd_log);
+    newtree->Branch("Jet_mass_brd_log", &Jet_mass_brd_log);
+
     Long64_t n_events = chain->GetEntries();
 
     for (Long64_t i = 0; i < n_events; ++i) {
         chain->GetEntry(i);
+
+        Float_t eps = 6;
 
         // Best Jet
         Jet_eta_bst = (nJet > 0) ? Jet_eta[0] : 0.0f;
         Jet_pt_bst = (nJet > 0) ? Jet_pt[0] : 0.0f;
         Jet_phi_bst = (nJet > 0) ? Jet_phi[0] : 0.0f;
         Jet_mass_bst = (nJet > 0) ? Jet_mass[0] : 0.0f;
+
+        Jet_eta_bst_log = std::log(Jet_eta_bst + eps);
+        Jet_pt_bst_log = std::log(Jet_pt_bst + eps);
+        Jet_phi_bst_log = std::log(Jet_phi_bst + eps);
+        Jet_mass_bst_log = std::log(Jet_mass_bst + eps);
         
         // Second best Jet
         Jet_eta_bnd = (nJet > 1) ? Jet_eta[1] : 0.0f;
         Jet_pt_bnd = (nJet > 1) ? Jet_pt[1] : 0.0f;
         Jet_phi_bnd = (nJet > 1) ? Jet_phi[1] : 0.0f;
         Jet_mass_bnd = (nJet > 1) ? Jet_mass[1] : 0.0f;
+
+        Jet_eta_bnd_log = std::log(Jet_eta_bnd + eps);
+        Jet_pt_bnd_log = std::log(Jet_pt_bnd + eps);
+        Jet_phi_bnd_log = std::log(Jet_phi_bnd + eps);
+        Jet_mass_bnd_log = std::log(Jet_mass_bnd + eps);
         
         // Third best Jet
         Jet_eta_brd = (nJet > 2) ? Jet_eta[2] : 0.0f;
         Jet_pt_brd = (nJet > 2) ? Jet_pt[2] : 0.0f;
         Jet_phi_brd = (nJet > 2) ? Jet_phi[2] : 0.0f;
         Jet_mass_brd = (nJet > 2) ? Jet_mass[2] : 0.0f;
+
+        Jet_eta_brd_log = std::log(Jet_eta_brd + eps);
+        Jet_pt_brd_log = std::log(Jet_pt_brd + eps);
+        Jet_phi_brd_log = std::log(Jet_phi_brd + eps);
+        Jet_mass_brd_log = std::log(Jet_mass_brd + eps);
     
         newtree->Fill();
     }
